e240df55e3be844374953cc062aa0829d66473bb
Kajol Asabe <kajol_asabe@persistent.com>
2021-01-01 17:50:39 +0530

    hook changes

--- main.go
+++ main.go
@@ -7,24 +7,32 @@ import (
 	_ "myproject/routers"
 
 	beego "github.com/beego/beego/v2/server/web"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/tus/tusd/cmd/tusd/cli/hooks"
 	"github.com/tus/tusd/pkg/filestore"
 	tusd "github.com/tus/tusd/pkg/handler"
 )
 
 func main() {
 
-	store := filestore.FileStore{
-		Path: "/opt/SLB-uploads",
+	hookHandler := &hooks.FileHook{
+		Directory: "/home/kajol/go/src/myproject/hooks",
 	}
 
 	composer := tusd.NewStoreComposer()
-	store.UseIn(composer)
-
-	handler, err := tusd.NewHandler(tusd.Config{
+	config := tusd.Config{
 		BasePath:              "/files/",
 		StoreComposer:         composer,
 		NotifyCompleteUploads: true,
-	})
+	}
+	//hookHandler.InvokeHook(hooks.HookPreCreate, tusd.Handler., true)
+
+	store := filestore.FileStore{
+		Path: "/opt/SLB-uploads",
+	}
+	store.UseIn(composer)
+
+	handler, err := tusd.NewHandler(config)
 	if err != nil {
 		panic(fmt.Errorf("Unable to create handler: %s", err))
 	}
@@ -32,10 +40,12 @@ func main() {
 	go func() {
 		for {
 			event := <-handler.CompleteUploads
-			fmt.Printf("Upload %s finished\n", event.Upload.ID)
+			fmt.Printf("Upload %s (%d bytes) finished\n", event.Upload.ID, event.Upload.Size)
+			hookHandler.InvokeHook(hooks.HookPostFinish, event, false)
 		}
 	}()
 
 	beego.Handler("/files/", http.StripPrefix("/files/", handler), true)
+	beego.Handler("/metrics", promhttp.Handler(), true)
 	beego.Run()
 }
